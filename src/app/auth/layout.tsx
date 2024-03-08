aee6c42b5d32f6e6a6bafa090f54b2d95dc6d655
stywn04 <stywn.jar@gmail.com>
2024-03-08 15:48:10 +0700

    feat: auth routes

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <div className="w-10/12 md:w-8/12 lg:w-5/12">{children}</div>
+    </div>
+  );
+}
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
+
+export default function RegisterPage() {
+  return (
+    <main>
+      <h1>Register Page!</h1>
+    </main>
+  );
+}
