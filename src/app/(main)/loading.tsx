77809d17f97cc4cf9ea7bd82ec55e56fb0bbb31f
stywn04 <stywn.jar@gmail.com>
2024-03-12 06:53:23 +0700

    feat: simple loading page

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,9 @@
+import { SubmitLoading } from "@/components/submit-loading";
+
+export default function Loading() {
+  return (
+    <div className="py-10">
+      <SubmitLoading />
+    </div>
+  );
+}
