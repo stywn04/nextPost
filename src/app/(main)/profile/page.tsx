d5c0fb5f8390f1c74b0be98560cfcf966cdfca48
stywn04 <stywn.jar@gmail.com>
2024-03-12 08:16:58 +0700

    feat: loading animation while fetching user posts

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,15 +1,8 @@
-import { getPostByUserId } from "@/app/actions/post.action";
 import { getCurrentUserAction } from "@/app/actions/user.action";
 import { ButtonUpdateUser } from "@/components/auth/update-user";
-import {
-  PostUser,
-  PostCount,
-  PostActivity,
-  PostDate,
-  PostContent,
-  PostOptions,
-} from "@/components/post";
-import Link from "next/link";
+import { SubmitLoading } from "@/components/submit-loading";
+import { UserPosts } from "@/components/user-posts";
+import { Suspense } from "react";
 
 export async function generateMetadata() {
   const { username, name } = await getCurrentUserAction();
@@ -21,7 +14,6 @@ export async function generateMetadata() {
 
 export default async function ProfilePage() {
   const user = await getCurrentUserAction();
-  const posts = await getPostByUserId();
   return (
     <main>
       <section className="grid grid-cols-12 gap-4 justify-between items-start">
@@ -48,47 +40,15 @@ export default async function ProfilePage() {
         username={user.username}
         bio={user.bio}
       />
-      <section className="py-10 flex flex-col gap-5">
-        {posts.length < 1 && (
-          <div className="py-5 text-slate-700">
-            you don't have any post,{" "}
-            <Link className="text-slate-400" href={"/create"}>
-              create one
-            </Link>
+      <Suspense
+        fallback={
+          <div className="py-10">
+            <SubmitLoading />
           </div>
-        )}
-        {posts.map((post) => (
-          <div key={post.id} className="p-5 border-b-[1px] border-slate-900">
-            <section className="flex items-start justify-between ">
-              <PostUser
-                avatar={post.user?.avatar as string}
-                name={post.user?.name as string}
-                username={post.user?.username as string}
-              />
-              <PostOptions />
-            </section>
-            <section className="ml-5 pl-7 border-l-[1px] border-slate-900 my-2">
-              <PostContent
-                post_id={post.id}
-                image={post.image}
-                content={post.content}
-              />
-              <PostActivity
-                user_id={user.id}
-                post_id={post.id}
-                like={post.like}
-              />
-            </section>
-            <section className="flex items-center justify-between text-sm text-slate-700 ml-5">
-              <PostCount
-                like={post.like.length}
-                comment={post.comment.length}
-              />
-              <PostDate created_at={post.created_at} />
-            </section>
-          </div>
-        ))}
-      </section>
+        }
+      >
+        <UserPosts />
+      </Suspense>
     </main>
   );
 }
