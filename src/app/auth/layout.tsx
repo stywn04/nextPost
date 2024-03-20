c492e0e3f8d709e0a167704d7c9f67a0fe5a2a62
stywn04 <stywn.jar@gmail.com>
2024-03-20 13:47:34 +0700

    fix: minor styling

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
       <div className="w-10/12 md:w-8/12 lg:w-5/12">{children}</div>
     </div>
   );
